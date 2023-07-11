a517edeb83be68b5613dfb58732b6bc3b37baf7b
HoangMy1112 <nhm8606@gmail.com>
2023-07-11 09:07:21 +0700

    StarUI

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import pytz
 
+
 # Create your models here.
 class Product(models.Model):
     seller = models.ForeignKey(User, on_delete = models.CASCADE)
